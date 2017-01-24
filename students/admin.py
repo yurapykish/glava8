68958fafd13f4cdb560f67aac053c824c765abc9
yura <yura_pykish1994@mail.ru>
2017-01-24 16:51:25 +0000

    pagination - error

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Student
-
-
+from model import Group , Student
 # Register your models here.
+admin.site.register(Group)
 admin.site.register(Student)
---
+++ students/model.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+#Student model
+class Group (models.Model):
+        
+	class Meta(object):
+  	    verbose_name=u'Групи'
+      	    verbose_name_plural=u'Групи'
+
+	title = models.CharField(
+	    max_length = 256,
+	    blank = False,
+            verbose_name = u'Назва ГРУПИ')
+	leader = models.OneToOneField('Student', verbose_name=u'Староста',
+            max_length = 256,
+            blank=True,
+	    null=True,
+	    on_delete=models.SET_NULL)
+	notes = models.TextField(
+	    blank=True,
+	    verbose_name=u'Додаткові нотатки')
+	
+	def __unicode__(self):
+	    if self.leader:
+
+	      return u'%s(%s %s) %s' % (self.title, self.leader.first_name, self.leader.last_name)
+	    else:
+	      return u"%s" % (self.title,)
+class Student (models.Model):
+
+	class Meta(object):
+	    verbose_name=u'Студенти',
+	    verbose_name_plural= u'Студенти'
+
+
+	first_name = models.CharField(
+	    max_length=256,
+	    blank=False,
+	    verbose_name=u'Ім*я')
+
+	last_name = models.CharField(
+	    max_length=256,
+	    blank=False,
+	    verbose_name=u'Прізвище')
+
+	middle_name = models.CharField(
+	    max_length = 256, 
+	    blank=True,
+	    verbose_name= u'По-батькові',
+	    default='')
+
+	student_group = models.ForeignKey('Group',
+	   verbose_name = u'Група',
+	   blank = False, 
+	   null = True,
+ 	   on_delete=models.PROTECT)
+	
+	birthday = models.DateField(
+	    blank=True,
+	    verbose_name=u'Дата народження',
+	    null=True)
+	
+	photo = models.ImageField(
+	    blank=True,
+	    verbose_name=u'Фото',
+	    null=True)
+	
+	ticket = models.CharField(
+	    max_length=256,	
+   	    blank=False,
+	    verbose_name=u'Білет')
+ 
+        notes = models.TextField(
+	    blank=True,
+	    verbose_name=u'Додаткові нотатки')
+	
+	def __unicode__(self):
+	    return u'%s %s' % (self.first_name, self.last_name)
+
+
