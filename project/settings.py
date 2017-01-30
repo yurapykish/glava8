b11444988c4cd07751c7980bb13a78d581e7afba
yura <yura_pykish1994@mail.ru>
2017-01-30 14:53:51 +0000

    error

--- project/settings.py
+++ project/settings.py
@@ -72,8 +72,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
